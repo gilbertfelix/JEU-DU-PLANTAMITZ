7010960cb5597e43f5c3d97f92d97328ee16ec44
Dorline21 <dorlineokassie237@gmail.com>
2025-12-11 11:35:52 +0100

    Mise à jour de main.c :

--- JEU-DU-PLANTAMITZ/main.c
+++ JEU-DU-PLANTAMITZ/main.c
@@ -5,11 +5,17 @@ int main() {
 
     char plateau[L][C];
 
-    // Remplir et vérifier le plateau
+    // Remplir le plateau au hasard
     remplir_plateau(plateau);
-    regenerer_si_alignement(plateau);
 
-    printf("Plateau initial 25x45 sans alignement automatique :\n");
+    // Vérifier et régénérer si des alignements interdits existent
+    if (alignement_existe(plateau)) {
+        printf("Alignements détectés, régénération du plateau...\n");
+        regenerer_si_alignement(plateau);
+    }
+
+    // Affichage du plateau final
+    printf("\nPlateau initial %dx%d sans alignement automatique :\n\n", L, C);
     afficher_plateau(plateau);
 
     return 0;
