02247ebcd5c7270f8d09d2c8ed4ce907af5cee54
FredVShub <dfrederic401@gmail.com>
2025-12-24 00:06:48 +0100

    modification

---
+++ JEU-DU-PLANTAMITZ/gestion_contrats.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void Regles_niveau1()
+{
+    printf("Félicitation pour votre parcours");
+    printf("Bienvenu au Niveau 1\n");
+    printf("objectif : Atteindre 400 points\n");
+    printf("Nombre de coups maximum : 50\n");
+    printf("Bonne chance à vous");
+}
+void Regles_niveau2()
+{
+    printf("Félicitation pour votre parcours\n");
+    printf(" Bienvenu au Niveau 2\n");
+    printf("objectif 1: Atteindre 400 points\n");
+    printf("objectif 2: Collecter 10 soleils\n");
+    printf("objectif 3: collecter 10 oignons\n");
+    printf("Nombre de coups maximum : 70\n");
+    printf("Bonne chance à vous");
+}
+void Regles_niveau3()
+{
+    printf("Félicitation votre progession est admirable\n");
+    printf("Bienvenu au Niveau 3\n");
+    printf("objectif 2: Collecter 40 soleils\n");
+    printf("objectif 3: collecter 50 oignons\n");
+    printf("Nombre de coups maximum : 20\n");
+    printf("Bonne chance à vous");
+}
