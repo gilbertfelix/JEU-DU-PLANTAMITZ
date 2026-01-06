5da7cf44a52fc0a8722258f4a6b66aa1f420ccad
gilbertfelix <125877738+gilbertfelix@users.noreply.github.com>
2026-01-06 21:23:29 +0100

    Ajout du fichier progression.c

---
+++ JEU-DU-PLANTAMITZ/progression.c
@@ -0,0 +1,38 @@
+include "header.h"
+
+void sauvegarder_progression(const char *nom_joueur,int niveau_atteint,int score){
+    File *fichier = fopen("plantamitz.txt", "w");
+
+    if (fichier == NULL){
+        printf("Erreur : impossible de sauvegarder la progression.\n");
+        return;
+    }
+
+    fprintf(fichier, "%s\n", nom_joueur);
+    fprintf(fichier, "%s\n", niveau_atteint);
+    fprintf(fichier, "%s\n", score);
+
+    fclose(fichier);
+    printf("\n Progression sauvegardée ! \n");
+}
+
+int charger_progression(char *nom_joueur, int *niveau_atteint, int *score){
+
+    FILE *fichier = fopen("plantamitz.txt", "r");
+
+    if(fichier == NULL){
+        return 0;
+    }
+
+    if(fscanf(fichier, "%49s\n%d\n%d", nom_joueur, niveau_atteint,score) != 3){
+        fclose(fichier);
+        return0;
+    }
+
+    fclose(fichier);
+    return 1;
+}
+
+void supprimer_sauvegarde(){
+    remove("plantamitz.txt");
+}
